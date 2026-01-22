61df66103409ede81ef42213b13f9dda0edc5145
junior <bogdangirichev125@gmail.com>
2026-01-22 13:35:09 +0500

    Доделать пожалуйста main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,30 +4,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DocHub</title>
+    <link rel="stylesheet" href="{{asset('css/app.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Geologica:wght,CRSV@100..900,0&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
 </head>
 
 <body>
-    <header class="header-shelf">
-        <div class="header-content">
-            <div class="logo">
-                <p>DocHub</p>
-            </div>
+    <header class="header">
+        <div>
 
-            <div class="search-container">
-                <input type="text" placeholder="Поиск документов...">
-            </div>
-
-            <div class="actions">
-                <button class="icon-btn"></button> <button class="icon-btn"></button>
-            </div>
         </div>
     </header>
+
+    <main class="main">
+
+        <div class="profile_card" id="profile_card">
+            <img src="{{asset('images/avatar.png') }}" alt="avatar" class="avatar_image">
+            <p class="name_text">Lorem Ips.</p>
+        </div>
+        <div class="lesson" id="lesson">
+
+        </div>
+        <div class="event" id="event">
+
+        </div>
+        <div class="menu" id="menu">
+
+        </div>
+        <div class="news" id="news">
+
+        </div>
+        <div class="message" id="message">
+
+        </div>
+
+    </main>
+
 </body>
 
 </html>
