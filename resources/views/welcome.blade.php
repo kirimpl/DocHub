af20af0823728f494a3bc8d5f26781a61b92a0b0
junior <bogdangirichev125@gmail.com>
2026-01-23 10:44:35 +0500

    Сделал адаптив под 1366

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -192,7 +192,7 @@
                 <span class="nav-text">Коллеги</span>
             </a>
 
-            <a href="#" class="nav-item">
+            <a href="messenger" class="nav-item">
                 <span class="nav-icon"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path
